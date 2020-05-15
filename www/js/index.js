252c30a8b263cb4e91c6db14bf56dd27faf92c87
PongPong <pongpong@Jupiter.local>
2020-05-15 19:21:16 +0800

    run on ios

--- www/js/index.js
+++ www/js/index.js
@@ -1,10 +1,5 @@
 var app = {
-  state = {
-    error: '',
-    status: 'Loading...',
-    product1: {},
-    product2: {},
-  },
+  state: {},
   setState: function (attr) {
     Object.assign(this.state, attr);
     this.render(this.state);
@@ -20,28 +15,37 @@ var app = {
 
   // deviceready Event Handler
   onDeviceReady: function () {
+    const self = this;
+
+    this.setState({
+      error: '',
+      status: 'Loading...',
+      product1: {},
+      product2: {},
+    });
+
     store.register([{
-        id:    'my_subscription1',
+        id:    'beady.purchasetest.subscription',
         type:   store.PAID_SUBSCRIPTION,
     }, {
-        id:    'my_subscription2',
+        id:    'beady.purchasetest.baseicsubscription',
         type:   store.PAID_SUBSCRIPTION,
     }]);
 
     // Setup the receipt validator service.
-    store.validator = '<<< YOUR_RECEIPT_VALIDATION_URL >>>';
+    // store.validator = '<<< YOUR_RECEIPT_VALIDATION_URL >>>';
 
     // Show errors for 10 seconds.
     store.error(function(error) {
-        this.setState({ error: `ERROR ${error.code}: ${error.message}` });
+        self.setState({ error: `ERROR ${error.code}: ${error.message}` });
         setTimeout(function() {
-            this.setState({ error: `` });
+            self.setState({ error: `` });
         }, 10000);
     });
 
     store.when('subscription').updated(function() {
-        const product1 = store.get('my_subscription1') || {};
-        const product2 = store.get('my_subscription2') || {};
+        const product1 = store.get('beady.purchasetest.subscription') || {};
+        const product2 = store.get('beady.purchasetest.baseicsubscription') || {};
     
         let status = 'Please subscribe below';
         if (product1.owned || product2.owned)
@@ -49,7 +53,7 @@ var app = {
         else if (product1.state === 'approved' || product2.state === 'approved')
             status = 'Processing...';
     
-        setState({ product1, product2, status });
+        self.setState({ product1, product2, status });
     });
 
     store.refresh();
@@ -57,14 +61,14 @@ var app = {
 
   render: function () {
     const purchaseProduct1 = this.state.product1.canPurchase
-      ? `<button onclick="store.order('my_subscription1')">Subscribe</button>`
+      ? `<button onclick="store.order('beady.purchasetest.subscription')">Subscribe</button>`
       : "";
     const purchaseProduct2 = this.state.product2.canPurchase
-      ? `<button onclick="store.order('my_subscription2')">Subscribe</button>`
+      ? `<button onclick="store.order('beady.purchasetest.baseicsubscription')">Subscribe</button>`
       : "";
 
-    const body = document.getElementsByTagName("body")[0];
-    body.innerHTML = `
+    const content = document.getElementById("content");
+    content.innerHTML = `
         <pre> 
             ${this.state.error}
             subscription: ${this.state.status}
